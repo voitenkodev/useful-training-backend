bc57d5e9b9e26fff18aeebec6bf51e80e2e5a2ea
maxvoitenko <voitenko.dev@gmail.com>
2023-03-19 23:06:14 +0200

    upd

--- README.md
+++ README.md
@@ -1,16 +1,11 @@
 # useful-training-backend
-Backend for Useful Training
 
-To Create .Jar
-```
-./gradlew :buildFatJar
-```
-
-To run Container
-```
-docker-compose up --build
-```
-To Check is ran (using app name)
-```
-docker exec -t -i usefultraining-gradle-app -t installDist
-```
+## Create local docker container
+* To Create `.Jar` File
+    ```
+    ./gradlew :buildFatJar
+    ```
+* To create and run Container
+    ```
+    docker-compose up --build
+    ```
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
 val exposed_version: String by project
+val postgresql_version: String by project
 
 plugins {
     application
@@ -23,9 +24,12 @@ dependencies {
     implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
+
     implementation("ch.qos.logback:logback-classic:$logback_version")
+
     implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
-    implementation("org.postgresql:postgresql:42.5.1")
+
+    implementation("org.postgresql:postgresql:$postgresql_version")
 }
